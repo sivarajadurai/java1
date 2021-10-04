3aa9cd16ff5f154a2fdbb55f5926b4e51b8e1be9
Siva Rajadurai <rajaduraisivagmail.com>
2021-10-04 11:13:51 +0530

    Pass the object value in parameterized method

--- src/com/prog/basic/Application.java
+++ src/com/prog/basic/Application.java
@@ -1,9 +1,34 @@
 package com.prog.basic;
 
+
+
 public class Application {
+	
+	void showDate(DateDisplay k){
+		
+		System.out.println("you time is :");
+		k.display();
+		
+	}
+	
+	void showString(String s){
+		int a = s.length();
+		System.out.println(a);
+	}
 
 	public static void main(String[] args) {
-		System.out.println("welcome");	
+		
+		
+		DateDisplay dd = new DateDisplay();
+		dd.sec =45;
+		dd.min =34;
+		dd.hour=5;
+		
+		Application a = new Application();
+		a.showDate(dd);
+		String name ="siva";
+		a.showString(name);
+		
 	}
 
 }
---
+++ src/com/prog/basic/DateDisplay.java
@@ -0,0 +1,11 @@
+package com.prog.basic;
+
+public class DateDisplay {
+	int sec;
+	int min;
+	int hour;
+	void display(){
+		System.out.println(hour +":"+min+":"+sec);
+	}
+
+}
